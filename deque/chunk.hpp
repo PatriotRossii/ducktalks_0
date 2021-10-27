291746901979d3df68d041624276b428bf71ebca
PatriotRossii <patriotrossii2019@mail.ru>
2021-10-27 20:45:31 +0500

    Add support for reversed/direct chunk

--- deque/chunk.hpp
+++ deque/chunk.hpp
@@ -4,11 +4,17 @@
 #include <initializer_list>
 
 template <typename T, std::size_t N> class chunk {
+  public:
+    enum class type {
+        reversed,
+        direct
+    };
+  private:
     std::vector<T> data;
     std::size_t size_{0};
-
+    type type_;
   public:
-    chunk() = default;
+    chunk(type t): type_{t} { }
     chunk(std::initializer_list<T> list) : data{list}, size_{list.size()} {}
     template <typename Iterator> chunk(Iterator from, Iterator to) {
         std::size_t idx = 0;
@@ -18,9 +24,9 @@ template <typename T, std::size_t N> class chunk {
         size_ = idx;
     }
 
-    auto operator[](std::size_t idx) const -> const T & { return data[N - idx - 1]; }
-    auto operator[](std::size_t idx) -> T & { return data[N - idx - 1]; }
+    auto operator[](std::size_t idx) const -> const T & { return (type_ == type::reversed ? data[N - idx - 1] : data[idx]); }
+    auto operator[](std::size_t idx) -> T & { return (type_ == type::reversed ? data[N - idx - 1] : data[idx]); }
 
-    auto begin() const -> const T * { return data.begin(); }
-    auto end() const -> const T * { return data.end(); }
+    auto begin() const -> const T * { return (type_ == type::reversed ? data.rbegin() : data.begin()); }
+    auto end() const -> const T * { return (type_ == type::reversed ? data.rend() : data.end()); }
 };
