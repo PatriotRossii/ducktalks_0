b9ec90e380b0c7e4e7b3a9130f48546a51eaa1bf
PatriotRossii <patriotrossii2019@mail.ru>
2021-10-27 18:52:32 +0500

    Rename chunk.h, fix typos, fmt

---
+++ deque/chunk.hpp
@@ -0,0 +1,23 @@
+#include <array>
+#include <initializer_list>
+
+template <typename T, size_t N> class chunk {
+    std::array<T, N> data;
+    std::size_t size_;
+  public:
+    chunk() {}
+    chunk(std::initializer_list<T> list) : data{list}, size_{list.size()} {}
+    template <typename Iterator> chunk(Iterator from, Iterator to) {
+        std::size_t idx = 0;
+        for (; from != to; ++from, ++idx) {
+            data[idx] = *from;
+        }
+        size_ = idx;
+    }
+
+    const T &operator[](std::size_t idx) const { return data[idx]; }
+    T &operator[](std::size_t idx) { return data[idx]; }
+
+    const T *begin() const { return data.begin(); }
+    const T *end() const { return data.end(); }
+};
