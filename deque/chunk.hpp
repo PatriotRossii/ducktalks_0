278d9c997f1d99cc21ed90c8c47a7de1be33ab3d
PatriotRossii <patriotrossii2019@mail.ru>
2021-10-27 20:51:59 +0500

    Integrate reversed/direct chunks...

--- deque/chunk.hpp
+++ deque/chunk.hpp
@@ -3,18 +3,18 @@
 #include <vector>
 #include <initializer_list>
 
+enum ChunkType {
+	reversed,
+	direct
+};
+
 template <typename T, std::size_t N> class chunk {
-  public:
-    enum class type {
-        reversed,
-        direct
-    };
-  private:
     std::vector<T> data;
     std::size_t size_{0};
-    type type_;
+    ChunkType type_;
   public:
-    chunk(type t): type_{t} { }
+	chunk(): chunk(ChunkType::direct) { }
+    chunk(ChunkType t): type_{t} { }
     chunk(std::initializer_list<T> list) : data{list}, size_{list.size()} {}
     template <typename Iterator> chunk(Iterator from, Iterator to) {
         std::size_t idx = 0;
@@ -24,9 +24,13 @@ template <typename T, std::size_t N> class chunk {
         size_ = idx;
     }
 
-    auto operator[](std::size_t idx) const -> const T & { return (type_ == type::reversed ? data[N - idx - 1] : data[idx]); }
-    auto operator[](std::size_t idx) -> T & { return (type_ == type::reversed ? data[N - idx - 1] : data[idx]); }
+    auto operator[](std::size_t idx) const -> const T & { return (type_ == ChunkType::reversed ? data[N - idx - 1] : data[idx]); }
+    auto operator[](std::size_t idx) -> T & { return (type_ == ChunkType::reversed ? data[N - idx - 1] : data[idx]); }
 
-    auto begin() const -> const T * { return (type_ == type::reversed ? data.rbegin() : data.begin()); }
-    auto end() const -> const T * { return (type_ == type::reversed ? data.rend() : data.end()); }
+    auto begin() const -> const T * { return (type_ == ChunkType::reversed ? data.rbegin() : data.begin()); }
+    auto end() const -> const T * { return (type_ == ChunkType::reversed ? data.rend() : data.end()); }
+
+	auto size() const -> std::size_t {
+		return size_;
+	}
 };
--- deque/deque.hpp
+++ deque/deque.hpp
@@ -24,9 +24,9 @@ template <typename T, std::size_t CHUNK_SIZE = 8> class deque {
     }
     void allocate(Direction direction) {
         if (direction == Direction::LEFT) {
-            chunks.insert(chunks.begin(), chunk<T, CHUNK_SIZE>{});
+            chunks.insert(chunks.begin(), chunk<T, CHUNK_SIZE>{ChunkType::reversed});
         } else {
-            chunks.push_back(chunk<T, CHUNK_SIZE>{});
+            chunks.push_back(chunk<T, CHUNK_SIZE>{ChunkType::reversed});
         }
     }
 
