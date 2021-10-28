431cd86682a660b4ef7ce79724a78bbd29f0328b
PatriotRossii <patriotrossii2019@mail.ru>
2021-10-28 16:43:02 +0500

    Add new helper methods to the chunk

--- deque/chunk.hpp
+++ deque/chunk.hpp
@@ -7,34 +7,37 @@ enum ChunkType { reversed, direct };
 
 template <typename T, std::size_t N> class chunk {
     std::vector<T> data;
-    std::size_t size_{0};
-    ChunkType type_;
 
   public:
-    chunk() : chunk(ChunkType::direct) {}
-    chunk(ChunkType t) : type_{t} {}
-    chunk(std::initializer_list<T> list) : data{list}, size_{list.size()} {}
+    chunk() = default;
+    chunk(std::initializer_list<T> list) : data{list} {}
     template <typename Iterator> chunk(Iterator from, Iterator to) {
-        std::size_t idx = 0;
-        for (; from != to; ++from, ++idx) {
+        for (size_t idx = 0; from != to; ++from, ++idx) {
             data[idx] = *from;
         }
-        size_ = idx;
     }
 
-    auto operator[](std::size_t idx) const -> const T & {
-        return (type_ == ChunkType::reversed ? data[N - idx - 1] : data[idx]);
+    auto get(std::size_t idx, ChunkType type = ChunkType::direct) const
+        -> const T & {
+        return (type == ChunkType::reversed ? data[N - idx - 1] : data[idx]);
     }
-    auto operator[](std::size_t idx) -> T & {
-        return (type_ == ChunkType::reversed ? data[N - idx - 1] : data[idx]);
-    }
-
-    auto begin() const -> const T * {
-        return (type_ == ChunkType::reversed ? data.rbegin() : data.begin());
-    }
-    auto end() const -> const T * {
-        return (type_ == ChunkType::reversed ? data.rend() : data.end());
+    auto get(std::size_t idx, ChunkType type = ChunkType::direct) -> T & {
+        return (type == ChunkType::reversed ? data[N - idx - 1] : data[idx]);
     }
 
-    auto size() const -> std::size_t { return size_; }
+    auto begin(ChunkType type = ChunkType::direct) const -> const T * {
+        return (type == ChunkType::reversed ? data.rbegin() : data.begin());
+    }
+    auto begin(ChunkType type = ChunkType::direct) -> T * {
+        return (type == ChunkType::reversed ? data.rbegin() : data.begin());
+    }
+
+    auto end(ChunkType type = ChunkType::direct) const -> const T * {
+        return (type == ChunkType::reversed ? data.rend() : data.end());
+    }
+    auto end(ChunkType type = ChunkType::direct) -> T * {
+        return (type == ChunkType::reversed ? data.rend() : data.end());
+    }
+
+    auto size() const -> std::size_t { return data.size(); }
 };
